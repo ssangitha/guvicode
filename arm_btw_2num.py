3ffab73f247d3a7cf398007df58a661801e2c8f9
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-01-08 21:36:02 +0530

    Create arm_btw_2num.py

---
+++ arm_btw_2num.py
@@ -0,0 +1,13 @@
+a,k=map(int,input().split())
+m=a
+s=0
+i=""
+for n in range(a+1,k):
+  while n>0:
+    r=n%10
+    s=s+(r*r*r)
+    n=n//10
+  if(m==s):
+    i=i+str(s)+" "
+    s=0
+print(i.strip())
