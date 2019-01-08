14ca2f8205bd92a6b86bf5ddb39c0a994c794de9
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-01-08 21:19:35 +0530

    Update prime_btw_2num.py

--- prime_btw_2num.py
+++ prime_btw_2num.py
@@ -1,8 +1,8 @@
-#.......prime nums
+#.......prime nums....
 s=""
 n,m=map(int,input().split())
 for i in range(n+1,m):
-  for j in range(2,i):
+  for j in range(2,i+1):
     if i%j==0:
       break
   if i==j:
