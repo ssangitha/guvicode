11fd0897f03177099548515affa1477377c71a43
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-01-08 21:50:51 +0530

    Update arm_btw_2num.py

--- arm_btw_2num.py
+++ arm_btw_2num.py
@@ -1,8 +1,8 @@
 a,k=map(int,input().split())
-m=a
 s=0
 i=""
 for n in range(a+1,k):
+  m=n
   while n>0:
     r=n%10
     s=s+(r*r*r)
@@ -11,5 +11,7 @@ for n in range(a+1,k):
     i=i+str(s)+" "
     s=0
   else:
-    s=0
-print(i.strip())
+  	s=0
+x=i.strip()
+print(x)
+#......arm....
