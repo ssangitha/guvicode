3c0cced0bd9e5d6c4039c1ab8fb62947e72d876c
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-01-08 20:58:47 +0530

    Update prime_btw_2num.py

--- prime_btw_2num.py
+++ prime_btw_2num.py
@@ -2,11 +2,10 @@
 s=""
 n,m=map(int,input().split())
 for i in range(n+1,m):
-  for j in range(2,i+1):
-    if i%j!=0 and i==j:
-      s=s+str(i)+" "
-    else:
+  for j in range(2,i):
+    if i%j==0:
       break
-if i==j:
-  r=s.strip()
-  print(r)
+  if i==j:
+    s=s+str(i)+" "
+r=s.strip()
+print(r)
