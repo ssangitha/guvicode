4148390c5cc1a82aba99b0c96f6583f52d9d8b4e
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-01-09 10:56:06 +0000

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,8 +1,10 @@
+
 #.....sort..........
+s=""
 n=int(input())
 l=list(map(int,input().split()))
 min=l[0]
-for i in range(1,n):
-  if min>l[i]:
-    l[i],l[i+1]=l[i+1],l[i]
-print(l)
+l.sort()
+for i in l:
+	s=s+str(i)+" "
+print(s.strip())
