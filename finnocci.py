f7a6b096b491e6aae005073069df0819b01b63aa
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-01-10 23:05:01 +0530

    Update finnocci.py

--- finnocci.py
+++ finnocci.py
@@ -1,10 +1,12 @@
 # fibbcc.......
 n=int(input())
+s=""
 a=1
 b=1
-print(a,b,end=" ")
+s=str(a)+" "+str(b)
 for i in range(2,n):
 	c=a+b
-	print(c,end=" ")
+	s=s+" "+str(c)
 	a=b
 	b=c
+print(s.strip())
