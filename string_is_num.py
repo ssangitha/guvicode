ca5ac55a9b5350563d7c1b9eb5d72c1be387294d
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-01-10 14:09:06 +0530

    Update string_is_num.py

--- string_is_num.py
+++ string_is_num.py
@@ -2,8 +2,8 @@ a=['0','1','2','3','4','5','6','7','8','9','.']
 s=input()
 for i in range(0,len(s)):
   if s[i] not in a:
-    print("no")
+    print("No")
     break
 if(i==(len(s)-1)):
-  print("yes")
+  print("Yes")
 #........num.....
