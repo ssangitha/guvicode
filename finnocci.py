323737ac8aeb5912b1fab9b8f56e57f6349b7bb7
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-01-10 23:09:39 +0530

    Update finnocci.py

--- finnocci.py
+++ finnocci.py
@@ -3,10 +3,14 @@ n=int(input())
 s=""
 a=1
 b=1
-s=str(a)+" "+str(b)
-for i in range(2,n):
-	c=a+b
-	s=s+" "+str(c)
-	a=b
-	b=c
-print(s.strip())
+if n==1:
+	print("1")
+else:
+	s=str(a)+" "+str(b)
+	for i in range(2,n):
+		c=a+b
+		s=s+" "+str(c)
+		a=b
+		b=c
+	print(s.strip())
+
