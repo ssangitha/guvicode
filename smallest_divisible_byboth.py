2583cc5f2a08cfbbe41b9c121f6cfff43e91cda1
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-01-30 15:44:32 +0530

    Update smallest_divisible_byboth.py

--- smallest_divisible_byboth.py
+++ smallest_divisible_byboth.py
@@ -1,6 +1,15 @@
 n,m=map(int,input().split())
 if n%m==0 or m%n==0:
 	print(max(n,m))
+elif n%10==0 and m%10==0:
+	n=str(n)
+	m=str(m)
+	a=max(len(n),len(m))
+	s=n[0]
+	x=m[0]
+	q=int(s)*int(m)
+	print(q+(0*(a-1)))
 else:
 	print(m*n)
+
 #....smallest divisible by both number...
