d9dc1fedbf13cd43ef5e536b384dafa10f94f48c
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-02-05 22:21:38 +0530

    Create right_shift.py

---
+++ right_shift.py
@@ -0,0 +1,18 @@
+n,k=map(int,input().split())
+l=list(map(int,input().split()))
+s=[0 for i in range(n)]
+x=""
+for i in range(0,n):
+	if i+k<len(l):
+		s[i+k]=l[i]
+	else:
+		if len(l)>1:
+			a=abs((len(l)-1)-i)
+			b=abs(a-k)
+			s[b-1]=l[i]
+		else:
+			s=l
+		
+for j in s:
+	x=x+str(j)+" "
+print(x.strip())
