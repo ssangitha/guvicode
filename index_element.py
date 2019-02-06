626ee3eaab3085ea598037e28e92b55d2bca961b
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-02-06 15:55:24 +0530

    Create index_element.py

---
+++ index_element.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=list(map(int,input().split()))
+s=[]
+x=""
+for i in range(len(l)):
+	if i==l[i]:
+		s.append(l[i])
+if len(s)==0:
+	print("-1")
+else:
+	for i in s:
+		x=x+str(i)+" "
+	print(x.strip())
+#.........print the elements if index equal to value..
