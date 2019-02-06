d3dcabf4b003f2a41207d68f5466b4cbafc1cb33
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-02-06 09:48:49 +0530

    Create unique.py

---
+++ unique.py
@@ -0,0 +1,12 @@
+n=int(input())
+x=""
+l=list(map(int,input().split()))
+s=[]
+for i in l:
+	if l.count(i)>1 and i not in s:
+		s.append(i)
+s.sort()
+for i in s:
+	x=x+str(i)+" "
+print(x.strip())
+#...unique....
