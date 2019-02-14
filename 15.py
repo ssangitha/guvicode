d6bebcff2b6377d1efa30babac60475793c0130d
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-02-14 21:33:18 +0530

    Create 15.py

---
+++ 15.py
@@ -0,0 +1,17 @@
+c=0
+n=int(input())
+l=list(map(int,input().split()))
+l1=[]
+for i in l:
+	d=i
+	while(d!=0):
+		d=d&(d-1)
+		c=c+1
+	l1.append(c)
+	c=0
+for i in range(0,len(l)-1):
+	for j in range(i+1,len(l)):
+		if l1[i]<l1[j]:
+			l1[i],l1[j]=l1[j],l1[i]
+			l[i],l[j]=l[j],l[i]
+print(l)
