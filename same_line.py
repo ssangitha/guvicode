aff7cd6d44102d123a503e27e122419de2ec98ac
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-02-14 22:45:48 +0530

    Create same_line.py

---
+++ same_line.py
@@ -0,0 +1,7 @@
+l1=list(map(int,input().split()))
+l2=list(map(int,input().split()))
+l3=list(map(int,input().split()))
+if (l1[0]==l2[0] and l2[0]==l3[0]) or (l1[1]==l2[1] and l2[1]==l3[1]):
+	print("yes")
+else:
+	print("no")
