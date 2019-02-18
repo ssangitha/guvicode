8652a47479c064989b0d22233a290f46b35d2c92
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-02-18 21:39:07 +0530

    Update pro_3.py

--- pro_3.py
+++ pro_3.py
@@ -1,11 +1,15 @@
 x,y=map(str,input().split())
-i=1
-j=1
+i=0
+j=0
 z2=len(y)
+z1=len(x)
 if x==y:
 	print("0")
 else:
 	a=x.index(y[0])
+	if z2<z1:
+		x,y=y,x
+		z2,z1=z1,z2
 	s=x[a:]
 	z=len(s)
 	while i<len(s) and j<len(s):
