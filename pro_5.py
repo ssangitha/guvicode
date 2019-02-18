28f7acbb87d896a84005eedfc0175b90b81f228a
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-02-18 22:38:49 +0530

    Update pro_5.py

--- pro_5.py
+++ pro_5.py
@@ -1,16 +1,20 @@
 n,a,b=map(int,input().split())
-x=n//2
-s=0
-i=1
-j=1
-while s<x:
-	s=(a*i)+(b*j)
+if n %2!=0:
+	print("NO")
+else:
+	x=n//2
+	s=0
+	i=1
+	j=1
+	for i in range(1,x):
+		for j in range(1,x):
+			s=(a*i)+(b*j)
+			if s==x:
+				break
+		if s==x:
+			break
 	if s==x:
 		print("YES")
-		break
 	else:
-		i=i+1
-		j=j+1
-if s>x:
-	print("NO")
+		print("NO")
 #.....................
