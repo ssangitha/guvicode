932029d6b159222dd0db7d5159082323974cd8d3
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-03-16 15:11:18 +0530

    Update hunter_7.py

--- hunter_7.py
+++ hunter_7.py
@@ -1,9 +1,12 @@
 n=int(input())
 l=list(map(int,input().split()))
 z=[]
+a=""
 for i in range(n):
 	if i%2==0 and l[i]%2==1:
 		z.append(str(l[i]))
 	elif i%2==1 and l[i]%2==0:
 		z.append(str(l[i]))
-print(" ".join(z))
+for i in z:
+    a=a+z+" "
+print(a.strip())
