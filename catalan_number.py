97b38dc7f26914b181a408f0e5c1a74d947e94de
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-05-14 22:55:36 +0530

    Create catalan_number.py

---
+++ catalan_number.py
@@ -0,0 +1,17 @@
+def catalan(n): 
+    if n <=1 : 
+        return 1
+    res = 0 
+    for i in range(n): 
+        res += catalan(i) * catalan(n-i-1) 
+  
+    return res 
+s=int(input())
+l=[]
+x=""
+for i in range(s+1): 
+    b=catalan(i)
+    l.append(b)
+for i in l:
+	x=x+str(i)+" "	
+print(x.strip())
