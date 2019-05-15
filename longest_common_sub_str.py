77facf0b4617c3607bb01c2ed784a280a7e14538
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-05-15 12:21:42 +0530

    Update longest_common_sub_str.py

--- longest_common_sub_str.py
+++ longest_common_sub_str.py
@@ -1,6 +1,7 @@
 a=input()
 b=input()
 l=[]
+m=[]
 if a==b:
 	print(a)
 else:
@@ -10,4 +11,16 @@ else:
 			if c in a:
 				l.append([c,len(c)])
 	l.sort(key=lambda x:x[1],reverse=True)
-	print(l[0][0])
+	print(l)
+	if l[0][0].isalpha():
+		print(l[0][0])
+	else:
+		for i in range(len(l)):
+			if l[i][1]==l[i+1][1]:
+				m.append(l[i][0])
+				m.append(l[i+1][0])
+				print(m)
+			else:
+				break
+		m.sort(reverse=True)
+		print(m[0])
