37aa9e05cf49f94845ffaa968ee24a0059549294
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-05-15 15:11:02 +0530

    Update sort_div.py

--- sort_div.py
+++ sort_div.py
@@ -1,5 +1,7 @@
 a,b=map(int,input().split())
-s=" "
-l=list(map(str,input().split()))
+s=""
+l=list(map(int,input().split()))
 l.sort()
-print(s.join(l))
+for i in l:
+	s=s+str(i)+" "
+print(s.strip())
