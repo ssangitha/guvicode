e53ddeee4f29fa763b0be1e70035755d3cec7f26
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-05-17 23:00:12 +0530

    Update power_of_index_and_el.py

--- power_of_index_and_el.py
+++ power_of_index_and_el.py
@@ -1,9 +1,7 @@
 s=input()
 a=[]
-if s=="1":
-	print("1")
-else:
-	for i in range(len(s)):
-		c=int(s[i])**i
-		a.append(c)
-	print(sum(a))
+for i in range(len(s)):
+	c=int(s[i])**i
+	a.append(c)
+print(sum(a))
+#
