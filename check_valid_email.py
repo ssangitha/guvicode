4809f451146e578c9e05eedb545c02df64ba319c
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-05-17 13:53:50 +0530

    Update check_valid_email.py

--- check_valid_email.py
+++ check_valid_email.py
@@ -7,7 +7,7 @@ for i in range(len(s)-2):
 		b=i
 	if s[i]==".":
 		y=s[b+1:i]
-if m==1 and a==1 and len(x)>=3 and len(y)>=4 and s[len(s)-4:]==".com":
+if m==1 and a==1 and len(x)>=3 and len(y)<=5 and s[len(s)-4:]==".com":
 	print("YES")
 else:
 	print("NO")
