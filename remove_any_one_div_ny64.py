2d063fe82fa28b014a51438b6b8a2ddf625e2fb3
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-05-24 22:18:49 +0530

    Update remove_any_one_div_ny64.py

--- remove_any_one_div_ny64.py
+++ remove_any_one_div_ny64.py
@@ -1,14 +1,19 @@
 n=input()
-if int(n,2)%64==0:
-	print("yes")
-else:
-	p=1
+p=1
+for i in range(len(n)):
+	s=n[i:]
+	a=int(s,2)
+	if a%64==0:
+		print("yes")
+		p=0
+		break
+if(p==1):
 	for i in range(len(n)):
-		s=n[:i]+n[i+1:]
+		s=n[:1+1]
 		a=int(s,2)
 		if a%64==0:
 			print("yes")
 			p=0
-			break
-	if(p==1):
-		print("no")
+		    break
+if p==1:
+	print("no")
