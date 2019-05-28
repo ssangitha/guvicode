21094d7c7cde9f69a911791ce339a484ad7d2f4d
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-05-28 17:33:06 +0530

    Update just_greater_than_n_smallest_greater.py

--- just_greater_than_n_smallest_greater.py
+++ just_greater_than_n_smallest_greater.py
@@ -7,10 +7,13 @@ l=[]
 for i in range(len(x)):
 	l.append(int("".join(x[i])))
 l.sort()
-for i in range(len(l)-1):
-	if l[i]==int(n):
-		print(l[i+1])
-		c=1
-		break
-if c==0:
+if all(i==l[0] for i in l):
 	print("impossible")
+else:
+	for i in range(len(l)-1):
+		if l[i]==int(n):
+			print(l[i+1])
+			c=1
+			break
+	if c==0:
+		print("impossible")
