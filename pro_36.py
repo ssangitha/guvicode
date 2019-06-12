1a04d18d276ca5782260a3a6bc4203fc4f0d3232
ssangitha <36667059+ssangitha@users.noreply.github.com>
2019-06-12 11:37:50 +0530

    Create pro_36.py

---
+++ pro_36.py
@@ -0,0 +1,9 @@
+n=int(input())
+l=list(map(int,input().split()))
+c=0
+for i in range(len(l)):
+	for j in range(i+1,len(l)):
+		for k in range(j+1,len(l)):
+			if l[i]>l[j]>l[k]:
+				c+=1
+print(c)
